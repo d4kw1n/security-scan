e7898accbf9f0f5d959a4828fc33ec7563e4ec1a
shr3wd <tripm2112@gmail.com>
2025-05-21 12:10:44 +0700

    New commit

---
+++ main.test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('./app');
+describe('GET /', () => {
+    it('should return Hello World', async () => {
+        const res = await request(app).get('/');
+        expect(res.text).toBe('Hello World!');
+    });
+});
