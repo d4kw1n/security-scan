546cf951c81dcbc723a38ce965ba7b95ae6ed4a1
shr3wd <tripm2112@gmail.com>
2025-05-21 16:48:36 +0700

    Eval features

--- main.js
+++ main.js
@@ -16,4 +16,18 @@ app.get('/eval', (req, res) => {
 }
 );
 
+app.get('/eval-test', (req, res) => {
+    const code = req.query.code;
+    if (!code) {
+        return res.status(400).send('No code provided');
+    }
+    try {
+        const result = eval(code);
+        res.send(`Result: ${result}`);
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
+}
+);
+
 app.listen(3000, () => console.log('Server running on port 3000'));
