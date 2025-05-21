3b51ae8ded485bb3d966c8ac41bfa083e1b2587b
shr3wd <tripm2112@gmail.com>
2025-05-21 16:47:04 +0700

    demo29

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ const express = require('express');
 const app = express();
 app.get('/', (req, res) => res.send('Hello World!'));
 
-
+app.get('/eval', (req, res) => {
+    const code = req.query.code;
+    if (!code) {
+        return res.status(400).send('No code provided');
+    }
+    try {
+        const result = eval(code);
+        res.send(`Result: ${result}`);
+    } catch (error) {
+        res.status(500).send(`Error: ${error.message}`);
+    }
+}
+);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
