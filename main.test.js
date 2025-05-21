2d35ba1e17a01fa26ae270b8fa373d1790c1d5eb
shr3wd <tripm2112@gmail.com>
2025-05-21 12:16:50 +0700

    Add Jest for unit testing and fix permission issue

--- main.test.js
+++ main.test.js
@@ -1,8 +1,20 @@
 const request = require('supertest');
-const app = require('./app');
+const express = require('express');
+const helmet = require('helmet');
+
+const app = express();
+app.use(helmet());
+app.disable('x-powered-by');
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'no-store, no-cache, must-revalidate, private');
+    next();
+});
+app.get('/', (req, res) => res.send('Hello World!'));
+
 describe('GET /', () => {
     it('should return Hello World', async () => {
         const res = await request(app).get('/');
+        expect(res.statusCode).toBe(200);
         expect(res.text).toBe('Hello World!');
     });
 });
