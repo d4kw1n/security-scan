889af99d8ce6532cc5e5c04f4dd50a1c3bf4322c
shr3wd <tripm2112@gmail.com>
2025-05-22 22:15:08 +0700

    ci: update ci-pipeline.yml

--- routes/authRoute.js
+++
@@ -1,8 +0,0 @@
-const route = require("express").Router();
-const auth = require("../Controller/AuthCTL.js");
-
-route.post("/register", auth.register);
-route.post("/login", auth.login);
-route.post("/logout", auth.logout);
-
-module.exports = route;
